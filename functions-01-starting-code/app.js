4f74b2e3e56f960934853c35dae2049eb4c4c9c7
Eun <euncheol.jang@howoocast.com>
2022-11-23 09:40:11 +0900

    화살표 함수

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,80 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSOR = "SCISSOR";
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSOR}`, "").toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSOR) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+const getComputerChoice = function () {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSOR;
+  }
+};
+
+// const getWinner = function (cChoice, pChoice) {
+//   if (cChoice === pChoice) {
+//     return RESULT_DRAW;
+//   } else if (
+//     cChoice === ROCK && pChoice === PAPER ||
+//     cChoice === PAPER && pChoice === SCISSOR ||
+//     cChoice === SCISSOR && pChoice === ROCK
+//   ) {
+//     return RESULT_PLAYER_WINS;
+//   } else {
+//     return RESULT_COMPUTER_WINS;
+//   }
+// };
+
+const add = (a, b) => a + b;
+const add2 = function(a, b) {
+  return a + b;
+}
+
+const getWinner = (cChoice, pChoice) => {
+  return cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSOR) ||
+      (cChoice === SCISSOR && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+};
+// const start = function () {
+//   console.log("Game is starting...");
+// };
+
+// startGameBtn.addEventListener("click", start);
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is starting...");
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
+});
+// 익명함수에도 이름을 줄 수 있다.
+// startGameBtn.addEventListener("click", function startGame() {
+//   console.log("Game is starting...", age);
+// });
