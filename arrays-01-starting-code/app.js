bbd99104247c8fde709158ab0c692f7762c08577
Eun <euncheol.jang@howoocast.com>
2022-11-29 14:55:41 +0900

    배열 생성하기

---
+++ arrays-01-starting-code/app.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3,];
+console.log(numbers)
+
+// const moreNumbers = new Array(5);
+const moreNumbers1 = Array(5);
+const moreNumbers2 = Array(5, 2);
+console.log(moreNumbers)
+
+const yetMoreNumbers = Array.of(1, 2);
+console.log(yetMoreNumbers)
+
+const listItems = document.querySelectorAll("li");
+  console.log(listItems)
+
+const moreNumbers = Arrray.from("Hi!") 
+console.log(moreNumbers)
