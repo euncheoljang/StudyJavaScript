4c93dfbb98917039bdc4a5165eb848b9d29a0434
Eun <euncheol.jang@howoocast.com>
2022-11-29 16:19:03 +0900

    forEach()

--- arrays-01-starting-code/app.js
+++ arrays-01-starting-code/app.js
@@ -50,33 +50,50 @@
 // const removedElements = hobbies.splice(-1, 1);
 // console.log(removedElements)
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10,];
+// const testResults = [1, 5.3, 1.5, 10.99, -5, 10,];
 // const storedResults = testResults.slice(0, 2);
 // const storedResults = testResults.slice(-3, -1);
 // const storedResults = testResults.slice(2);
 
-// 배열 더하기
-const storedResults = testResults.concat([3.99, 2]);
+// // 배열 더하기
+// const storedResults = testResults.concat([3.99, 2]);
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults)
-console.log(testResults.indexOf(1.5, 1));
-console.log(testResults.lastIndexOf(1.5))
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(1.5, 1));
+// console.log(testResults.lastIndexOf(1.5));
+// console.log(testResults.includes(10.99));
+// console.log(testResults.indexOf(10.99) !== -1);
 
-const personData = [{name: "Max"}, {name: "Manuel"}];
-// 아무것도 못찾으면 -1 반황
-console.log(personData.indexOf({name: "Manuel"}))
+// const personData = [{name: "Max"}, {name: "Manuel"}];
+// // 아무것도 못찾으면 -1 반황
+// console.log(personData.indexOf({name: "Manuel"}));
 
-const manuel = personData.find((person, index, persons) => {
-  return person.name === "Manuel";
+// const manuel = personData.find((person, index, persons) => {
+//   return person.name === "Manuel";
+// });
+
+// manuel.name = "Anna";
+// console.log(manuel, personData)
+
+// const maxIndex = personData.findIndex((person, index, persons) => {
+//   return person.name === "Max";
+// });
+
+// console.log(maxIndex)
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdujustedPrices = [];
+
+// for (const price of prices) {
+//   taxAdujustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, index, prices) => {
+  const priceObj = {index: index, taxAdjPrice: price * (1 + tax)}
+  taxAdujustedPrices.push(priceObj)
 });
 
-manuel.name = "Anna";
-console.log(manuel, personData)
-
-const maxIndex = personData.findIndex((person, index, persons) => {
-  return person.name === "Max";
-});
-
-console.log(maxIndex)
+console.log(taxAdujustedPrices);
