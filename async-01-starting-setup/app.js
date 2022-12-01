7b88e5b50d39fdd7821a0b5dd7c6b08ca9e5c2d6
Eun <euncheol.jang@howoocast.com>
2022-12-01 17:19:23 +0900

    동기화 비동기화 실행 순서

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -2,7 +2,21 @@ const button = document.querySelector('button');
 const output = document.querySelector('p');
 
 function trackUserHandler() {
-  console.log('Clicked!');
+  navigator.geolocation.getCurrentPosition(posData => {
+    console.log(posData);
+  }, error => {
+    console.log(error);
+  });
+  console.log("Getting position...");
 }
 
+// trackUserHandler가 callback function임
+// 리스너는 브라우저가 관리
 button.addEventListener('click', trackUserHandler);
+
+// let result = 0
+// for (let i= 0;  i < 9999999999; i++) {
+//   result =+ i;
+// }
+
+// console.log(result);
