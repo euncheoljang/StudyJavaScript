418d060fddc07d3ee10a3cdd2161e6de5b51da47
Eun <euncheol.jang@howoocast.com>
2022-12-01 14:39:03 +0900

    지원되는 이벤트 유형

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -5,7 +5,7 @@ const buttons = document.querySelectorAll("button");
 // };
 
 const buttonClickHandler = event => {
-  event.target.disabled = true;
+  // event.target.disabled = true;
   console.log(event);
 };
 
@@ -24,6 +24,13 @@ const anotherButtonClickHandler = () => {
 //   button.removeEventListener("click", buttonClickHandler);
 // }, 2000);
 
+// buttons.forEach(btn => {
+//   btn.addEventListener("click", buttonClickHandler);
+// });
 buttons.forEach(btn => {
-  btn.addEventListener("click", buttonClickHandler);
+  btn.addEventListener("mouseenter", buttonClickHandler);
+});
+
+window.addEventListener("scroll", event => {
+  console.log(event);
 });
