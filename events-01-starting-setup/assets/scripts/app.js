34e33e62e9c5aa6aaee84fb150db324a7bb4c28a
Eun <euncheol.jang@howoocast.com>
2022-12-01 16:36:22 +0900

    데이터 요소 drop & move

--- events-01-starting-setup/assets/scripts/app.js
+++ events-01-starting-setup/assets/scripts/app.js
@@ -84,6 +84,7 @@ class ProjectItem {
     this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectMoreInfoButton();
     this.connectSwitchButton(type);
+    this.connectDrag();
   }
 
   showMoreInfoHandler() {
@@ -103,6 +104,13 @@ class ProjectItem {
     this.hasActiveTooltip = true;
   }
 
+  connectDrag() {
+    document.getElementById(this.id).addEventListener("dragstart", event => {
+      event.dataTransfer.setData("text/plain", this.id);
+      event.dataTransfer.effectAllowed = "move";
+    });
+  }
+  
   connectMoreInfoButton() {
     const projectItemElement = document.getElementById(this.id);
     const moreInfoBtn = projectItemElement.querySelector(
@@ -140,6 +148,45 @@ class ProjectList {
       );
     }
     console.log(this.projects);
+    this.connectDroppable();
+  }
+
+  connectDroppable() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+    
+    list.addEventListener("dragenter", event => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+      list.parentElement.classList.add("droppable");
+    });
+    
+    list.addEventListener("dragover", event => {
+      event.preventDefault();
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener("dragleave", event => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove("droppable");
+      }
+    });
+
+    list.addEventListener("drop", event => {
+      const prjId = event.dataTransfer.getData("text/plain");
+      if (this.projects.find(p => p.id === prjId)) {
+        return;
+      }
+      document
+        .getElementById(prjId)
+        .querySelector("button:last-of-type")
+        .click()
+        list.parentElement.classList.remove("droppable");
+        event.preventDefault(); // 기본동작을 방지할대 쓰임
+    });
+    
   }
 
   setSwitchHandlerFunction(switchHandlerFunction) {
