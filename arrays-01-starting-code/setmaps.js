ff9be3b991744b85dec0cdcb0b959430b369ef62
Eun <euncheol.jang@howoocast.com>
2022-12-01 11:43:27 +0900

    WeakSet()

--- arrays-01-starting-code/setmaps.js
+++ arrays-01-starting-code/setmaps.js
@@ -13,28 +13,33 @@
 // }
 
 
-const person1 = {name: "Max"};
-const person2 = {name: "Manuel"};
+// const person1 = {name: "Max"};
+// const person2 = {name: "Manuel"};
 
-const personData = new Map([[person1, [{data: "yesterday", price: 10}]]]);
+// const personData = new Map([[person1, [{data: "yesterday", price: 10}]]]);
 
-personData.set(person2, [{data: "two weeks ago", price: 100}]);
+// personData.set(person2, [{data: "two weeks ago", price: 100}]);
 
-console.log(personData);
-console.log(personData.get(person1));
+// console.log(personData);
+// console.log(personData.get(person1));
 
-// for (const entry of personData.entries()) {
-for (const [key, value] of personData.entries()) {
-  // console.log(entry);
-  console.log(key, value)
-}
+// // for (const entry of personData.entries()) {
+// for (const [key, value] of personData.entries()) {
+//   // console.log(entry);
+//   console.log(key, value)
+// }
 
-for (const key of personData.keys()) {
-  console.log(key);
-}
-for (const value of personData.values()) {
-  console.log(value);
-}
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
 
-console.log(personData.size)
+// console.log(personData.size)
 
+let person = {name: "Max"};
+const persons = new WeakSet();
+persons.add(person);
+
+console.log(persons);
