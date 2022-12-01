271e60a3d033dab389f68465c53729ad0d13494b
Eun <euncheol.jang@howoocast.com>
2022-12-01 15:12:41 +0900

    이벤트 전파 & stopPropagation()

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -1,4 +1,5 @@
-const buttons = document.querySelectorAll("button");
+// const buttons = document.querySelectorAll("button");
+const button = document.querySelector("button");
 
 // button.onclick = function() {
 
@@ -55,3 +56,33 @@ form.addEventListener("submit", event => {
   event.preventDefault();
   console.log(event);
 });
+
+
+// 기본적으로 이벤트는 버블링으로 실행 (안에서 밖으로, button -> div)
+const div = document.querySelector("div");
+
+div.addEventListener("click", event => {
+  console.log("Clicked DIV");
+  console.log(event);
+});
+
+button.addEventListener("click", event => {
+  event.stopPropagation();
+  console.log("Clicked BUTTON");
+  console.log(event);
+});
+
+
+// // 캡처링 이벤트 리스너
+// const div = document.querySelector("div");
+
+// div.addEventListener("click", event => {
+//   console.log("Clicked DIV");
+//   console.log(event);
+//   // 조상을 먼저 실행하고 싶으면 true 넣기
+// }, true);
+
+// button.addEventListener("click", event => {
+//   console.log("Clicked BUTTON");
+//   console.log(event);
+// });
