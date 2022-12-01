ae55685ce5803b80b29412be38e5f09edc890108
Eun <euncheol.jang@howoocast.com>
2022-12-01 15:49:37 +0900

    이벤트 핸들러 함수 & this

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -66,11 +66,11 @@ div.addEventListener("click", event => {
   console.log(event);
 });
 
-button.addEventListener("click", event => {
-  event.stopPropagation(); // default event가 막니는것은 아님, 조상 엘리먼트만 막음
-  console.log("Clicked BUTTON");
-  console.log(event);
-});
+// button.addEventListener("click", event => {
+//   event.stopPropagation(); // default event가 막니는것은 아님, 조상 엘리먼트만 막음
+//   console.log("Clicked BUTTON");
+//   console.log(event);
+// });
 
 const listItems = document.querySelectorAll("li");
 const list = document.querySelector("ul");
@@ -88,16 +88,18 @@ const list = document.querySelector("ul");
 //   event.target.classList.toggle("highlight");
 // });
 
-// 단점 복잡해지면 어려움
-list.addEventListener("click", event => {
-  // // currentTarget은 리스너가 적용된 요소를 참조함
-  // console.log(event.currentTarget);
+// // 단점 복잡해지면 어려움
+// list.addEventListener("click", event => {
+//   // // currentTarget은 리스너가 적용된 요소를 참조함
+//   // console.log(event.currentTarget);
 
-  // 가장 가까운 li를 찾음 클릭된 target이 li이더라도 li를 찾음
-  event.target.closest("li").classList.toggle("highlight");
-});
+//   // 가장 가까운 li를 찾음 클릭된 target이 li이더라도 li를 찾음
+//   event.target.closest("li").classList.toggle("highlight");
+// });
 
 // 전파여부는 bubble 프로퍼티로 확인 가능
+
+// ==============================================================================================
 
 // // 캡처링 이벤트 리스너
 // const div = document.querySelector("div");
@@ -112,3 +114,23 @@ list.addEventListener("click", event => {
 //   console.log("Clicked BUTTON");
 //   console.log(event);
 // });
+
+// ==============================================================================================
+
+list.addEventListener("click", function(event) {
+  // // currentTarget은 리스너가 적용된 요소를 참조함
+  // console.log(event.currentTarget);
+
+  // 가장 가까운 li를 찾음 클릭된 target이 li이더라도 li를 찾음
+  event.target.closest("li").classList.toggle("highlight");
+  // form.submit();
+  button.click();
+  console.log(this);
+});
+
+button.addEventListener("click", function(event) {
+  event.stopPropagation(); 
+  console.log("Clicked BUTTON");
+  console.log(event);
+  console.log(this);
+});
