f0ddb9badeffe203d705332c63292662d4ef27ac
Eun <euncheol.jang@howoocast.com>
2022-12-01 15:31:23 +0900

    이벤트 위임 사용하

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -67,11 +67,37 @@ div.addEventListener("click", event => {
 });
 
 button.addEventListener("click", event => {
-  event.stopPropagation();
+  event.stopPropagation(); // default event가 막니는것은 아님, 조상 엘리먼트만 막음
   console.log("Clicked BUTTON");
   console.log(event);
 });
 
+const listItems = document.querySelectorAll("li");
+const list = document.querySelector("ul");
+
+// // 리스너가 많아서 별로임
+// listItems.forEach(listItem => {
+//   listItem.addEventListener("click", event => {
+//     event.target.classList.toggle("highlight");
+//   });
+// });
+
+// 효율적인 방법
+// list.addEventListener("click", event => {
+//   // list에 이벤트리스너를 등록해도 target은 실제 클릭된 부분인 li를 가리킨다
+//   event.target.classList.toggle("highlight");
+// });
+
+// 단점 복잡해지면 어려움
+list.addEventListener("click", event => {
+  // // currentTarget은 리스너가 적용된 요소를 참조함
+  // console.log(event.currentTarget);
+
+  // 가장 가까운 li를 찾음 클릭된 target이 li이더라도 li를 찾음
+  event.target.closest("li").classList.toggle("highlight");
+});
+
+// 전파여부는 bubble 프로퍼티로 확인 가능
 
 // // 캡처링 이벤트 리스너
 // const div = document.querySelector("div");
