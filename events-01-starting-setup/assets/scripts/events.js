dc628cf08a51437779f1481505495456e5a267c9
Eun <euncheol.jang@howoocast.com>
2022-12-01 14:00:51 +0900

    다양한 이벤트리스너 등록 방법

---
+++ events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,22 @@
+const button = document.querySelector("button");
+
+// button.onclick = function() {
+
+// };
+
+// const buttonClickHandler = () => {
+//   alert("Button was clicked!");
+// }
+
+// const anotherButtonClickHandler = () => {
+//   console.log("Thie was clicked!");
+// };
+
+// // 오버라이드해서 어나더 핸들러만 실행됨
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickHandler;
+
+// 다중으로 리스너 가능
+button.addEventListener();
+
+button.removeEventListener();
