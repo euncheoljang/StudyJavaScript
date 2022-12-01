5310d631435c630f6600fe2a8b76dbd25f7aef93
Eun <euncheol.jang@howoocast.com>
2022-12-01 09:51:53 +0900

    Map()

--- arrays-01-starting-code/setmaps.js
+++ arrays-01-starting-code/setmaps.js
@@ -1,12 +1,40 @@
-const ids = new Set(["Hi", "from", "Set!"]);
-// 중복된 값은 안들어간다
-ids.add("from");
-ids.add("2");
-ids.delete("Hisdasdas");
-console.log(ids);
-console.log(ids.has(1));
+// const ids = new Set(["Hi", "from", "Set!"]);
+// // 중복된 값은 안들어간다
+// ids.add("from");
+// ids.add("2");
+// // 없는걸 지우려하거나 중복된 값을 넣으려하면 무시할뿐임 오류가 안남
+// ids.delete("Hisdasdas");
+// console.log(ids);
+// console.log(ids.has(1));
 
-for (const entry of ids.entries()) {
-  console.log(entry)
-  console.log(entry[0])
+// for (const entry of ids.entries()) {
+//   console.log(entry)
+//   console.log(entry[0])
+// }
+
+
+const person1 = {name: "Max"};
+const person2 = {name: "Manuel"};
+
+const personData = new Map([[person1, [{data: "yesterday", price: 10}]]]);
+
+personData.set(person2, [{data: "two weeks ago", price: 100}]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+// for (const entry of personData.entries()) {
+for (const [key, value] of personData.entries()) {
+  // console.log(entry);
+  console.log(key, value)
 }
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+for (const value of personData.values()) {
+  console.log(value);
+}
+
+console.log(personData.size)
+
