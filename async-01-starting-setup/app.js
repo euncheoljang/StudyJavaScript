a1a44c4437ca9644acf868daebbdc8d054f22927
Eun <euncheol.jang@howoocast.com>
2022-12-02 10:01:56 +0900

    프라미스 시작하기

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -1,26 +1,38 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const button = document.querySelector("button");
+const output = document.querySelector("p");
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
 
 function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(posData => {
-    setTimeout(() => {
-      console.log(posData);
-    }, 2000);
-  }, error => {
-    console.log(error);
-  });
+  navigator.geolocation.getCurrentPosition(
+    (posData) => {
+      setTimer(2000).then(data => {
+        console.log(data, posData)
+      });
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 
-  // 콜백함수가 실행되기 위해서는 항상 메시지큐와 이벤트 루프를 통과해야한다. 
+  // 콜백함수가 실행되기 위해서는 항상 메시지큐와 이벤트 루프를 통과해야한다.
   // 그래서 getting position... 이 먼저 실행
-  setTimeout(() => {
-    console.log("Timer done!");
-  }, 0);
+  setTimer(1000).then(() => {
+    console.log("Timer Done!");
+  });
   console.log("Getting position...");
 }
 
 // trackUserHandler가 callback function임
 // 리스너는 브라우저가 관리
-button.addEventListener('click', trackUserHandler);
+button.addEventListener("click", trackUserHandler);
 
 // let result = 0
 // for (let i= 0;  i < 9999999999; i++) {
