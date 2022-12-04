a0d8d7f01047ab2cd203fdc29f891349d0156e78
JangEuncheol <jangec65@gmail.com>
2022-12-04 23:10:52 +0900

    POST 요청으로 데이터 보내기

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -1,23 +1,49 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = "json";
+    xhr.onload = function () {
+      resolve(xhr.response);
+      // const listOfPosts = JSON.parse(xhr.response);
+    };
+    xhr.send(JSON.stringify(data));
+  });
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  return promise;
+}
 
-xhr.responseType = "json";
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const listOfPosts = responseData;
+  for (const post of listOfPosts) {
+    const posetEl = document.importNode(postTemplate.content, true);
+    posetEl.querySelector("h2").textContent = post.title.toUpperCase();
+    posetEl.querySelector("p").textContent = post.body;
+    listElement.append(posetEl);
+  }
+}
 
-xhr.onload = function() {
-    // const listOfPosts = JSON.parse(xhr.response);
-    const listOfPosts = xhr.response;
-    for (const post of listOfPosts) {
-        const posetEl = document.importNode(postTemplate.content, true)
-        posetEl.querySelector("h2").textContent = post.title.toUpperCase();
-        posetEl.querySelector("p").textContent = post.body;
-        listElement.append(posetEl);
-    }
+async function createPost(title, content) {
+    const userId = Math.random();
+    const post = {
+        titile: title,
+        body: content,
+        userId: userId
+    };
 
-};
+    sendHttpRequest(
+        "POST", 
+        "https://jsonplaceholder.typicode.com/posts",
+        post
+    )
+}
 
-xhr.send();
-
+fetchPosts();
+createPost("DUMMY", "A dummy post!");
