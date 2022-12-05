d74069237512ee8e8b20fc72a237418922c52c47
JangEuncheol <jangec65@gmail.com>
2022-12-05 22:58:13 +0900

    헤더 추가하기

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -30,7 +30,11 @@ function sendHttpRequest(method, url, data) {
   // return promise;
   return fetch(url, {
     method: method,
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      
+    }
   }).then(response => {
     return response.json();
   });
